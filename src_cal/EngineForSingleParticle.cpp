1cbf01e62a2b381b83bf5784d301498747409e51
ylang <ylang@pku.edu.cn>
2017-12-21 13:35:19 +0800

    Case-dependent lightSpeed, Engine-dependent _W

--- src_cal/EngineForSingleParticle.cpp
+++ src_cal/EngineForSingleParticle.cpp
@@ -18,9 +18,50 @@
 #include <indexCache.h>
 
 
+inline double _W1(double x){
+    if (x > 2)
+        return 0.0;
+    else if (x > 1)
+        return x * (Cube(x) * (x * (x * (x * (15.0/1024 * x - 15.0/128) + 49.0/128) - 21.0/32) + 35.0/64) - 1.0) + 1.0;
+    else if (x > 0)
+        return Square(x) * (Square(x) * (x * (x * (x * (-15.0/1024 * x - 15.0/128) + 7.0/16) - 21.0/32) + 175.0/256) - 105.0/128) + 337.0/512;
+    else if (x > -1)
+        return x * x * (x * x * (x * (x * (x * (-15.0/1024 * x + 15.0/128) + 7.0/16) + 21.0/32) + 175.0/256) - 105.0/128) + 337.0/512;
+    else if (x > -2)
+        return x * (Cube(x) * (x * (x * (x * (15.0/1024 * x + 15.0/128) + 49.0/128) + 21.0/32) + 35.0/64) + 1.0) + 1.0;
+    else
+        return 0.0;
+}
+
+inline double dW1(double x){
+    if (x > 2)
+        return 0.0;
+    else if (x > 1)
+        return Cube(x) * (x * (x * (x * (15.0/128 * x - 105.0/128) + 147.0/64) - 105.0/32) + 35.0/16) - 1.0;
+    else if (x > 0)
+        return x * (x * x * (x * (x * (x * (-15.0/128 * x - 105.0/128) + 21.0/8) - 105.0/32) + 175.0/64) - 105.0/64);
+    else if (x > -1)
+        return x * (x * x * (x * (x * (x * (-15.0/128 * x + 105.0/128) + 21.0/8) + 105.0/32) + 175.0/64) - 105.0/64);
+    else if (x > -2)
+        return Cube(x) * (x * (x * (x * (15.0/128 * x + 105.0/128) + 147.0/64) + 105.0/32) + 35.0/16) + 1.0;
+    else
+        return 0.0;
+}
 
 
-EngineForSingleParticle::EngineForSingleParticle(Grid *_grid, double dt): Engine(_grid, dt) {
+
+inline double _W(const Vector3D &r){
+    return _W1(r.x)*_W1(r.y)*_W1(r.z);
+}
+
+
+inline void GradW(Vector3D& gradW, const Vector3D &r){
+    gradW.x=dW1(r.x)*_W1(r.y)*_W1(r.z);
+    gradW.y=_W1(r.x)*dW1(r.y)*_W1(r.z);
+    gradW.z=_W1(r.x)*_W1(r.y)*dW1(r.z);
+}
+
+EngineForSingleParticle::EngineForSingleParticle(Grid *_grid, double dt, double lightSpeed):lightSpeed(lightSpeed), Engine(_grid, dt) {
 #if MPI_PARALLEL && USE_CACHE
     PtAdjacentL=Range(P_ADJ_BEGIN_L,SWAP_LEN,0,grid->_height,0,grid->_length);
 	PtAdjacentR=Range(grid->_width-SWAP_LEN,grid->_width-P_ADJ_BEGIN_R,0,grid->_height,0,grid->_length);
